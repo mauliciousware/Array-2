9b41b4a98250a8288940089478c1e833f1201dff
Shreemauli Raut <30777026+mauliciousware@users.noreply.github.com>
2025-05-31 16:03:25 -0400

    Completed

---
+++ Problem1.js
@@ -0,0 +1,7 @@
+// # Array-2
+
+// ## Problem1 (https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/)
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
---
+++ Problem2.js
@@ -0,0 +1,8 @@
+// ## Problem2
+// Given an array of numbers of length N, find both the minimum and maximum. Follow up : Can you do it using less than 2 * (N - 2) comparison
+
+// ## Problem3 (https://leetcode.com/problems/game-of-life/)
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
